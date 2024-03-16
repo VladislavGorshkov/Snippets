49416c61401ecf9480f446559576d8c9ff6e3efe
VladislavGorshkov <vlad110977@yandex.ru>
2024-03-16 11:24:24 +0300

    add snippet get and post

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,6 +1,17 @@
-from django.http import Http404
+from django.http import Http404, HttpResponse, HttpResponseNotFound
 from django.shortcuts import render, redirect
 from MainApp.models import Snippet
+from django.core.exceptions import ObjectDoesNotExist
+from django.forms import ModelForm
+from MainApp.models import Snippet
+
+
+class SnippetForm(ModelForm):
+   class Meta:
+       model = Snippet
+       # Описываем поля, которые будем заполнять в форме
+       fields = ['name', 'lang', 'code']
+
 
 
 def index_page(request):
@@ -9,12 +20,41 @@ def index_page(request):
 
 
 def add_snippet_page(request):
-    context = {'pagename': 'Добавление нового сниппета'}
-    return render(request, 'pages/add_snippet.html', context)
-
+    #Создаем пустую форму при запросе методом GET
+    if request.method =="GET":
+        form = SnippetForm()
+        context = {'pagename': 'Добавление нового сниппета',
+               'form':form,
+               }
+        return render(request, 'pages/add_snippet.html', context)
+    #Получаем данные из формы и на их основе создаем новый экземпляр
+    if request.method == "POST":
+       form = SnippetForm(request.POST)
+       if form.is_valid():
+           form.save()
+           return redirect("snippets_page")
+    return render(request,'add_snippet.html',{'form': form})
 
 def snippets_page(request):
     snippets = Snippet.objects.all()
     context={'pagename': 'Просмотр сниппетов',
             "snippets":snippets}
     return render(request, 'pages/view_snippets.html', context)
+
+def snippet_detail(request,snippet_id):
+    try:
+        snippet = Snippet.objects.get(id=snippet_id)
+    except ObjectDoesNotExist:
+        return HttpResponseNotFound(f"Snipprt{snippet_id} not fount")
+    context={'pagename': 'Просмотр сниппетов',
+            "snippet":snippet,}
+
+    return render(request, 'pages/snippet_detail.html', context)
+
+# def create_snippet(request):
+#    if request.method == "POST":
+#        form = SnippetForm(request.POST)
+#        if form.is_valid():
+#            form.save()
+#            return redirect("snippets_page")
+#        return render(request,'add_snippet.html',{'form': form})
